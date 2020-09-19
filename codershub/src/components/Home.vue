a56d040e773666c84943d6c9228b1b7b03d5f809
ZachyDev <mosetizachary001@gmail.com>
2020-09-20 02:11:28 +0300

    installed the vue/cli and set up a basic vue-router plugin✅✅

---
+++ codershub/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <h1>CodersHub Connect</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() { }
+}
+</script>
+
+<style>
+
+</style>
---
+++ codershub/src/components/Services.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="services">
+        <h1>What we do</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Services"
+}
+</script>
+
+<style>
+
+</style>
